ba3c53f60b1c2c8a5b183ec4afe0389e72f6ac3d
jinsu <devjinsu@gmail.com>
2018-06-26 20:52:33 -0500

    Initial commit

---
+++ app/src/main/java/com/jinsukim/pheramor/UserRegistrationActivity.java
@@ -0,0 +1,13 @@
+package com.jinsukim.pheramor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UserRegistrationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_registration);
+    }
+}
